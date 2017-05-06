53d3531cd8259c77c90148fb82a533661748c87a
Anton Kiyatkin <axlbant@sent.com>
2017-05-06 15:05:02 +0400

    Update update.php

--- update.php
+++ update.php
@@ -1,49 +1,35 @@
 <?php
-	namespace infrajs\mail;
-	use infrajs\access\Access;
-	use infrajs\ans\Ans;
-	use infrajs\path\Path;
-	use infrajs\load\Load;
+use infrajs\mail\Mail;
+use infrajs\access\Access;
+use infrajs\ans\Ans;
+use infrajs\path\Path;
+use infrajs\load\Load;
 
-	Access::test(true);
-	
-	if (!is_file('vendor/autoload.php')) {
-	    chdir('../../../'); //Согласно фактическому расположению файла
-		require_once('vendor/autoload.php');
-	}
-	ini_set("display_errors", 1);
-	
-	$from='noreplay@'.$_SERVER['HTTP_HOST'];
-	$headers='From: '.$from."\r\n";
-	$headers.="Content-type: text/plain; charset=UTF-8\r\n";
-	$headers.='Reply-To: aky@list.ru'."\r\n";
-	//echo 'Нативная проверка<br>';
-	//$r=mail('info@itlife-studio.ru','Проверка с сервера '.$_SERVER['HTTP_HOST'],'Текст проверочного сообщения',$headers);
-	//var_dump($r);
+Access::test(true);
 
-	//return;//нельзя зачастую лимит стоит сколько писем за раз можно отправлять
-	//echo '<br>Сложная проверка<br>';
+$from = 'noreplay@'.$_SERVER['HTTP_HOST'];
+$headers = 'From: '.$from."\r\n";
+$headers .= "Content-type: text/plain; charset=UTF-8\r\n";
+$headers .= 'Reply-To: aky@list.ru'."\r\n";
+//echo 'Нативная проверка<br>';
+//$r=mail('info@itlife-studio.ru','Проверка с сервера '.$_SERVER['HTTP_HOST'],'Текст проверочного сообщения',$headers);
+//var_dump($r);
 
-	$conf=Access::$conf['admin'];
+//return;//нельзя зачастую лимит стоит сколько писем за раз можно отправлять
+//echo '<br>Сложная проверка<br>';
+$ans = array();
+if (empty(Access::$conf['admin']['support'])) return Ans::err($ans, 'У администратора не указан email support');
 
-	$ans = array();
-	if (!$conf['support']) return Ans::err($ans,'У администратора не указан email support');
 
-	
-	$body = Path::theme('-mail/update.tpl');
-	$body = file_get_contents($body);
-	$body = str_replace( 
-		array("{host}", "{date}"), 
-		array($_SERVER['HTTP_HOST'], date('j.m.Y')), 
-		$body
-	);
-	$subject='Выполнено обновление '.$_SERVER['HTTP_HOST'];
-	$email_from='noreplay@'.$_SERVER['HTTP_HOST'];
-	$r = Mail::toSupport($subject, $email_from, $body);
-	
-	if (!$r) {
-		error_log('Не удалось отправить тестовое письмо.');
-		if(Access::debug()&&!Load::isphp()) {
-			echo '<pre>Не удалось отправить <a href="/-mail/update.php">тестовое письмо</a>.</pre>'."\n";
-		}
-	}
+$body = Path::theme('-mail/update.tpl');
+$body = file_get_contents($body);
+$body = str_replace( 
+	array("{host}", "{date}"), 
+	array($_SERVER['HTTP_HOST'], date('j.m.Y')), 
+	$body
+);
+$subject = 'Выполнено обновление '.$_SERVER['HTTP_HOST'];
+$email_from = 'noreplay@'.$_SERVER['HTTP_HOST'];
+$r = Mail::toSupport($subject, $email_from, $body);
+
+if (!$r) error_log('-mail: При обновлении не удалось отправить тестовое письмо.');
